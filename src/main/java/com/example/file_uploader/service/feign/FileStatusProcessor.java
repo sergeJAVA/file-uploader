b04e382b237976d1ba6fcffb92ddf5b91facf443
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-08-25 09:54:58 +0300

    feat: Конфигурация теперь берётся с конфиг-сервера.

--- src/main/java/com/example/file_uploader/service/feign/FileStatusProcessor.java
+++ src/main/java/com/example/file_uploader/service/feign/FileStatusProcessor.java
@@ -1,11 +1,9 @@
 package com.example.file_uploader.service.feign;
 
-import com.example.file_uploader.dto.CheckStatusRequest;
 import com.example.file_uploader.dto.FileDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
